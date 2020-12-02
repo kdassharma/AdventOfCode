809b3d68c1a1e506d3c611b6eb8779fa8c2d0b50
kdassharma <kaustav.dassharma@mail.mcgill.ca>
2020-12-02 00:57:28 -0500

    Day 2

--- Advent.java
+++ Advent.java
@@ -10,6 +10,8 @@ public class Advent{
         
         // day1Part1(); //=290784
         // day1Part2(); //=177337980
+        //day2Part1(); //=410
+        day2Part2(); //=
 
     } 
 
@@ -110,4 +112,100 @@ public class Advent{
 
     }
 
+    // How many passwords are valid according to their policies?
+    public static void day2Part1() {
+        int valid = 0;
+
+        BufferedReader reader;
+        try {
+			reader = new BufferedReader(new FileReader(
+					"./data/Day2.data"));
+			String line = reader.readLine();
+			while (line != null) {
+
+                if (confirmPolicy(line)) {
+                    valid++;
+                }
+				// read next line
+				line = reader.readLine();
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+        }
+        
+        System.out.println(valid);
+
+    }
+
+    public static boolean confirmPolicy(String s) {
+        String[] split = s.split(" ");
+        int[] range = findRange(split[0]);
+        char c = split[1].charAt(0);
+        int count = 0;
+
+        for (int i = 0; i<split[2].length(); i++) {
+            if (split[2].charAt(i) == c) {
+                count++;
+            }
+        }
+
+        return count <= range[1] && count >= range[0];
+    }
+
+    public static int[] findRange(String s) {
+        int[] out = {0,0};
+        String num1 = "";
+        String num2 = "";
+        boolean found = false;
+
+        for (int i = 0; i<s.length(); i++) {
+            if (s.charAt(i) == '-') {
+                found = true;
+            }
+            else if (found) {
+                num2 += s.charAt(i);
+            }
+            else {
+                num1 += s.charAt(i);
+            }
+        }
+
+        out[0] = Integer.parseInt(num1); 
+        out[1] = Integer.parseInt(num2); 
+        return out;
+    }
+
+     // How many passwords are valid according to their updated policies?
+    public static void day2Part2() {
+        int valid = 0;
+
+        BufferedReader reader;
+        try {
+			reader = new BufferedReader(new FileReader(
+					"./data/Day2.data"));
+			String line = reader.readLine();
+			while (line != null) {
+
+                if (confirmUpdatedPolicy(line)) {
+                    valid++;
+                }
+				// read next line
+				line = reader.readLine();
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+        }
+        
+        System.out.println(valid);
+    }
+
+    public static boolean confirmUpdatedPolicy(String s) {
+        String[] split = s.split(" ");
+        int[] range = findRange(split[0]);
+        char c = split[1].charAt(0);
+
+        return (split[2].charAt(range[0] - 1) == c &&  split[2].charAt(range[1] - 1) != c) || (split[2].charAt(range[0] - 1) != c &&  split[2].charAt(range[1] - 1) == c);
+    }
 }
